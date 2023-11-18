c082330597ca072751e9595eb45590f69554128d
ngan <vutran@sheridancollege.ca>
2023-11-18 14:39:54 -0500

    Delete Note

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -13,7 +13,7 @@ export default function Sidebar(props) {
                 <h4 className="text-snippet">{note.body.split("\n")[0]}</h4>
                 <button 
                     className="delete-btn"
-                    onClick={(e) => props.trashIconClickEvent(e, note.id)}
+                    onClick={() => props.trashIconClickEvent(note.id)}
                 >
                     <i className="gg-trash trash-icon"></i>
                 </button>
