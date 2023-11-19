28ba08bec707e94a298b81983286f2d9b13a9d14
ngan <vutran@sheridancollege.ca>
2023-11-19 08:23:27 -0500

    createdAt and updatedAt

--- App.jsx
+++ App.jsx
@@ -14,7 +14,9 @@ export default function App() {
     
     async function createNewNote() {
         const newNote = {
-            body: "# Type your markdown note's title here"
+            body: "# Type your markdown note's title here", 
+            createdAt: Date.now(),
+            updatedAt: Date.now()
         }
         const newNoteRef = await addDoc(notesCollection, newNote)
         setCurrentNoteId(newNoteRef.id)
@@ -22,7 +24,7 @@ export default function App() {
     
     async function updateNote(text) {
         const docRef = doc(db, "notes", currentNoteId)
-        await setDoc(docRef, {body: text}, {merge: true})
+        await setDoc(docRef, {body: text, updatedAt: Date.now()}, {merge: true})
     }
     
     React.useEffect(() => {
